ff58ab24d8102477e35e183426a3c15a98e5ca53
Dastan Akhmetzhanov <58333215+dakhmetz@users.noreply.github.com>
2019-12-25 22:01:03 +0500

    feat: add form validation

--- src/js/main.js
+++ src/js/main.js
@@ -86,6 +86,101 @@ $(document).ready(function () {
   var bullets = $('.swiper-pagination');
 
   next.css('left', prev.width() + 10 + bullets.width() + 10);
-  bullets.css('left', prev.width() + 10)
+  bullets.css('left', prev.width() + 10);
+
+  new WOW().init();
+
+  // Валидация формы
+
+  $('.modal__form').validate({
+    errorClass: "invalid",
+    rules: {
+      userName: {
+        required: true,
+        minlength: 2
+      },
+      userPhone: {
+        required: true,
+        minlength: 16
+      },
+      userEmail: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Должно быть больше 2-х символов"
+      },
+      userPhone: {
+        required: "Имя обязательно",
+        minlength: "Неверная длина телефона"
+      },
+      userEmail: {
+        required: "Email обязателен",
+        email: "Формат email должен содержать name@domain.com"
+      }
+    }
+  });
+
+  $('.control__form').validate({
+    errorClass: "invalid",
+    rules: {
+      userName: {
+        required: true,
+        minlength: 2
+      },
+      userPhone: {
+        required: true,
+        minlength: 16
+      },
+    },
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Должно быть больше 2-х символов"
+      },
+      userPhone: {
+        required: "Имя обязательно",
+        minlength: "Неверная длина телефона"
+      },
+    }
+  });
+
+  $('.footer__form').validate({
+    errorClass: "invalid",
+    rules: {
+      userName: {
+        required: true,
+        minlength: 2
+      },
+      userPhone: {
+        required: true,
+        minlength: 16
+      },
+      userQuestion: {
+        required: true,
+        minlength: 10
+      }
+    },
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Должно быть больше 2-х символов"
+      },
+      userPhone: {
+        required: "Имя обязательно",
+        minlength: "Неверная длина телефона"
+      },
+      userQuestion: {
+        required: "Вопрос обязателен",
+        minlength: "Длина вопроса должна быть не менее 5 символов"
+      }
+    }
+  });
+
+  //Маска для телефона
+  $('[type=tel]').mask('+7(000)000-00-00', {placeholder: "+7(___)___-__-__"});
 
 });
